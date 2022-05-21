665b8741013c3da0c24e30d6dc64d2d4d1d8f29b
Masterchef365 <duncan.freeman1@gmail.com>
2022-05-20 23:41:12 -0700

    Blend cube

---
+++ examples/blend_cube.rs
@@ -0,0 +1,79 @@
+use idek::{prelude::*, IndexBuffer, MultiPlatformCamera};
+
+fn main() -> Result<()> {
+    launch::<_, TriangleApp>(Settings::default().vr_if_any_args())
+}
+
+struct TriangleApp {
+    verts: VertexBuffer,
+    indices: IndexBuffer,
+    shader: Shader,
+    camera: MultiPlatformCamera,
+}
+
+impl App for TriangleApp {
+    fn init(ctx: &mut Context, platform: &mut Platform, _: ()) -> Result<Self> {
+        let (vertices, indices) = rainbow_cube();
+        Ok(Self {
+            verts: ctx.vertices(&vertices, false)?,
+            indices: ctx.indices(&indices, false)?,
+            shader: ctx.shader(
+                DEFAULT_VERTEX_SHADER,
+                DEFAULT_FRAGMENT_SHADER,
+                Primitive::Triangles,
+                Blend::Additive,
+            )?,
+            camera: MultiPlatformCamera::new(platform),
+        })
+    }
+
+    fn frame(&mut self, _ctx: &mut Context, _: &mut Platform) -> Result<Vec<DrawCmd>> {
+        Ok(vec![DrawCmd::new(self.verts)
+            .indices(self.indices)
+            .shader(self.shader)])
+    }
+
+    fn event(
+        &mut self,
+        ctx: &mut Context,
+        platform: &mut Platform,
+        mut event: Event,
+    ) -> Result<()> {
+        if self.camera.handle_event(&mut event) {
+            ctx.set_camera_prefix(self.camera.get_prefix())
+        }
+        idek::close_when_asked(platform, &event);
+        Ok(())
+    }
+}
+
+fn rainbow_cube() -> (Vec<Vertex>, Vec<u32>) {
+    let vertices = vec![
+        Vertex::new([-1.0, -1.0, -1.0], [0.0, 1.0, 1.0]),
+        Vertex::new([1.0, -1.0, -1.0], [1.0, 0.0, 1.0]),
+        Vertex::new([1.0, 1.0, -1.0], [1.0, 1.0, 0.0]),
+        Vertex::new([-1.0, 1.0, -1.0], [0.0, 1.0, 1.0]),
+        Vertex::new([-1.0, -1.0, 1.0], [1.0, 0.0, 1.0]),
+        Vertex::new([1.0, -1.0, 1.0], [1.0, 1.0, 0.0]),
+        Vertex::new([1.0, 1.0, 1.0], [0.0, 1.0, 1.0]),
+        Vertex::new([-1.0, 1.0, 1.0], [1.0, 0.0, 1.0]),
+    ];
+
+    let indices = vec![
+        3, 1, 0, 2, 1, 3, 2, 5, 1, 6, 5, 2, 6, 4, 5, 7, 4, 6, 7, 0, 4, 3, 0, 7, 7, 2, 3, 6, 2, 7,
+        0, 5, 4, 1, 5, 0,
+    ];
+
+    let indices = indices
+        .iter()
+        .copied()
+        .chain(
+            indices
+                .chunks_exact(3)
+                .map(|win| [win[2], win[1], win[0]])
+                .flatten(),
+        )
+        .collect();
+
+    (vertices, indices)
+}
